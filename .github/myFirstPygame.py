43efec0f9d3dd34b4b83b7a5e0efe6325ec7fccb
Vsullivan0620 <s11268257@students.duvalschools.org>
2021-11-30 14:39:40 -0500

    v0.3
    Made my own Color

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -9,3 +9,11 @@ pygame.init()
 # Setup the game window.
 windowSurface = pygame.display.set_mode((500, 400),0, 32)
 pygame.display.set_caption('Hello, world!')
+
+#Setup Color values.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+MAGNETBLUE = (52, 186, 235)
